7e08f629a642f8c9698dce9afde1b0923268f525
VrentJBM <vrentmanuel@gmail.com>
2023-10-13 05:41:37 +0800

    Added Files for grading system website

---
+++ repo_vm/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ComponentsModule } from './components/components.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutModule } from './layout/layout.module';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginModule } from './login/login.module';
+import { RegistrationModule } from './registration/registration.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    LoginModule,
+    RegistrationModule,
+    ComponentsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ repo_vm/src/app/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { ScreenComponent } from '../components/screen/screen.component';
+import { LayoutComponent } from './layout.component';
+import { TopnavComponent } from '../components/topnav/topnav.component';
+import { SidenavComponent } from '../components/sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    ScreenComponent,
+    LayoutComponent,
+    TopnavComponent,
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
